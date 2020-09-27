04d19748779f5f2eb435cea8562b5f45d7017978
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-09-27 19:53:34 -0400

    version v1.1.0

--- index.ts
+++ index.ts
@@ -1,84 +1,92 @@
-import * as http from 'http'
-import finalhandler from 'finalhandler'
-import serveStatic from 'serve-static'
-import open  from "open"
+import finalhandler from "finalhandler";
+import serveStatic from "serve-static";
+import open from "open";
+import * as http from "http";
 
-export function entry({ StatusBarItem, Notification, RunningConfig, ContextMenu }: any) {
-  let launchedServers = {}
+export function entry(
+  { StatusBarItem, Notification, RunningConfig, ContextMenu }: any) {
+
+  let launchedServers = {};
 
   const config = {
     port: 5520,
     hint: "List live servers",
     anyFolderHint: "Open some folder",
     inactive: {
-      text: "[⚡ Run]",
+      text: "⚡ Run",
     },
     active: {
-      text: "[🔌 Dispose]",
+      text: "🔌 Dispose",
     },
   };
-  
-  const anyFolderOpened =  () => RunningConfig.data.workspaceConfig.folders.length > 0
-  
+
+  const anyFolderOpened = () =>
+    RunningConfig.data.workspaceConfig.folders.length > 0;
+
   const BarItem = new StatusBarItem({
-    label: 'Live Server',
+    label: "Live Server",
     hint: config.anyFolderHint,
     action(e: MouseEvent) {
-     
-      if(!anyFolderOpened()) return
-      
+      if (!anyFolderOpened()) {
+        return;
+      }
+
       new ContextMenu({
         event: e,
-        parent:e.target,
-        list: RunningConfig.data.workspaceConfig.folders.map(({ name, path }) => {
-          
-          const status = launchedServers[path] ? config.active.text : config.inactive.text
-          
-          return {
-            label: `${status} -> ${name}`,
-            action(){
-              const isRunning = !!launchedServers[path]
+        parent: e.target,
+        list: RunningConfig.data.workspaceConfig.folders.map(
+          ({ name, path }) => {
+            const status = launchedServers[path]
+              ? config.active.text
+              : config.inactive.text;
 
-              if(isRunning){
-                launchedServers[path].stop()
-                launchedServers[path] = null
-              }else{
-                const serverInstance = runServer(path)
-                launchedServers[path] = serverInstance
-                launchedServers[path].run()
-              }
-            }
+            return {
+              label: `${status} -> ${name}`,
+              action() {
+                const isRunning = !!launchedServers[path];
+
+                if (isRunning) {
+                  launchedServers[path].stop();
+                  launchedServers[path] = null;
+                }
+                else {
+                  const serverInstance = runServer(path);
+                  launchedServers[path] = serverInstance;
+                  launchedServers[path].run();
+                }
+              },
+            };
           }
-        })
-      })
+        ),
+      });
     },
   });
-  
-  RunningConfig.on('addFolderToRunningWorkspace',() => {
-    BarItem.setHint(config.hint)
-  })
-  
+
+  RunningConfig.on("addFolderToRunningWorkspace", () => {
+    BarItem.setHint(config.hint);
+  });
+
   function runServer(dir: string) {
     const serve = serveStatic(dir);
 
-    const server = http.createServer(function(req, res) {
+    const server = http.createServer(function (req, res) {
       const done = finalhandler(req, res);
+      // @ts-ignore
       serve(req, res, done);
     });
 
-  
     new Notification({
-      title: 'LiveServer',
-      content: `Serving ${dir} on port ${config.port}`
-    })
+      title: "LiveServer",
+      content: `Serving on port ${config.port}`,
+    });
 
-    setTimeout(()=>{
+    setTimeout(() => {
       open(`http://localhost:${config.port}`);
-    },500)
+    }, 500);
 
     return {
-      run : () =>  server.listen(config.port),
-      stop : () => server.close()
-    }
+      run: () => server.listen(config.port),
+      stop: () => server.close(),
+    };
   }
 }
